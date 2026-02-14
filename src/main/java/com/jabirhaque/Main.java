86cd52149a4d092a39cbc03e3d0ef75f91e41753
jabirhaque <jabirhaque@hotmail.co.uk>
2026-02-14 18:31:21 +0000

    adding slab block initialisation and allocation methods

--- src/main/java/com/jabirhaque/Main.java
+++ src/main/java/com/jabirhaque/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         OffHeapSlabAllocator offHeapSlabAllocator = new OffHeapSlabAllocator();
         offHeapSlabAllocator.printInfo();
-        long ptr = offHeapSlabAllocator.allocateMemory(1024);
-        System.out.println(ptr);
     }
 }
--- src/main/java/com/jabirhaque/OffHeapSlabAllocator.java
+++ src/main/java/com/jabirhaque/OffHeapSlabAllocator.java
@@ -3,23 +3,71 @@ package com.jabirhaque;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
+import java.util.Stack;
 
 public class OffHeapSlabAllocator {
 
     private final Unsafe unsafe;
+    private final long totalSize;
+    private final long blockSize;
+    private final long blockCount;
+
+    private Stack<Long> freeBlocks = new Stack<>();
+
+    public OffHeapSlabAllocator(long totalSize, long blockSize) throws NoSuchFieldException, IllegalAccessException {
+        this.unsafe = getUnsafe();
+        this.totalSize = totalSize;
+        this.blockSize = blockSize;
+        this.blockCount = totalSize/blockSize;
+        initialiseBlocks();
+    }
+
+    public OffHeapSlabAllocator(long totalSize) throws NoSuchFieldException, IllegalAccessException {
+        this.unsafe = getUnsafe();
+        this.totalSize = totalSize;
+        this.blockSize = 64;
+        this.blockCount = totalSize/blockSize;
+        initialiseBlocks();
+    }
 
     public OffHeapSlabAllocator() throws NoSuchFieldException, IllegalAccessException {
+        this.unsafe = getUnsafe();
+        this.totalSize = 16 * 1024 * 1024;
+        this.blockSize = 64;
+        this.blockCount = totalSize/blockSize;
+        initialiseBlocks();
+    }
+
+    private void initialiseBlocks(){
+        long baseAddress = unsafe.allocateMemory(totalSize);
+        long current = baseAddress;
+        long limit = baseAddress+totalSize;
+        while (current+blockSize <= limit ){
+            freeBlocks.push(current);
+            current += blockSize;
+        }
+    }
+
+    public long allocate(long bytes) throws IllegalArgumentException {
+        if (bytes > blockSize){
+            throw new IllegalArgumentException("Requested size exceeds block size");
+        }
+        return allocateBlock();
+    }
+
+    private long allocateBlock(){
+        if (freeBlocks.empty()) throw new OutOfMemoryError("Out of blocks");
+        return freeBlocks.pop();
+    }
+
+    private static Unsafe getUnsafe() throws NoSuchFieldException, IllegalAccessException {
         Field f = Unsafe.class.getDeclaredField("theUnsafe");
         f.setAccessible(true);
-        this.unsafe = (Unsafe) f.get(null);;
+        return (Unsafe) f.get(null);
     }
 
     public void printInfo(){
         System.out.println("Address size: " + unsafe.addressSize());
         System.out.println("Page size: " + unsafe.pageSize());
-    }
-
-    public long allocateMemory(long bytes){
-        return unsafe.allocateMemory(bytes);
     }
 }
