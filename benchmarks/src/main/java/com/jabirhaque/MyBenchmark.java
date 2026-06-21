f2a4996b114cc87f36b74d212351e1b1fcd04bc3
Jabir Nurul Haque <jabirhaque@hotmail.co.uk>
2026-06-21 15:16:45 +0100

    slab append and free latency

--- benchmarks/src/main/java/com/jabirhaque/MyBenchmark.java
+++ benchmarks/src/main/java/com/jabirhaque/MyBenchmark.java
@@ -2,39 +2,65 @@ package com.jabirhaque;
 
 import org.openjdk.jmh.annotations.*;
 
-@State(Scope.Thread)
 public class MyBenchmark {
 
-    private OffHeapSlabAllocator allocator;
-    private long address;
+    @State(Scope.Thread)
+    public static class FreeState {
 
-    @Setup(Level.Trial)
-    public void setup() throws NoSuchFieldException, IllegalAccessException {
-        allocator = new OffHeapSlabAllocator(
-                16 * 1024 * 1024,
-                64
-        );
+        OffHeapSlabAllocator allocator;
+        long address;
+
+        @Setup(Level.Trial)
+        public void setup() throws NoSuchFieldException, IllegalAccessException {
+            allocator = new OffHeapSlabAllocator(
+                    16 * 1024 * 1024,
+                    64
+            );
+        }
+
+        @Setup(Level.Invocation)
+        public void prepareFree() {
+            address = allocator.allocate(64);
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown() {
+            allocator.close();
+        }
     }
 
-    @Setup(Level.Invocation)
-    public void prepareFree() {
-        address = allocator.allocate(64);
-    }
+    @State(Scope.Thread)
+    public static class AllocateState {
 
-    @TearDown(Level.Trial)
-    public void tearDown() {
-        allocator.close();
-    }
+        OffHeapSlabAllocator allocator;
+        long address;
 
-//    @Benchmark
-//    public long allocate() {
-//        long addr = allocator.allocate(64);
-//        allocator.free(addr); // clean up
-//        return addr;
-//    }
+        @Setup(Level.Trial)
+        public void setup() throws NoSuchFieldException, IllegalAccessException {
+            allocator = new OffHeapSlabAllocator(
+                    16 * 1024 * 1024,
+                    64
+            );
+        }
+
+        @TearDown(Level.Invocation)
+        public void completeAllocate() {
+            allocator.free(address);
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown() {
+            allocator.close();
+        }
+    }
 
     @Benchmark
-    public void free() {
-        allocator.free(address);
+    public void free(FreeState state) {
+        state.allocator.free(state.address);
+    }
+
+    @Benchmark
+    public void allocate(AllocateState state) {
+        state.address = state.allocator.allocate(64);
     }
 }
