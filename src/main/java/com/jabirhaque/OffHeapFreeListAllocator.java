4759381351e41b6efad7e72a0b72a013f98d84af
Jabir Nurul Haque <jabirhaque@hotmail.co.uk>
2026-08-05 22:22:44 +0100

    adding splitting in the allocator - lots of magic numbers, add variables

--- src/main/java/com/jabirhaque/OffHeapFreeListAllocator.java
+++ src/main/java/com/jabirhaque/OffHeapFreeListAllocator.java
@@ -26,11 +26,22 @@ public class OffHeapFreeListAllocator implements OffHeapAllocator{
     @Override
     public long allocate(long bytes){
         long current = baseAddress;
-        while (current < baseAddress + totalSize && (unsafe.getByte(current) == 0 || unsafe.getLong(current+Byte.BYTES) < bytes)){
-            current = unsafe.getLong(current+Byte.BYTES+2*Long.BYTES);
+        while (current < baseAddress + totalSize && (unsafe.getByte(current) == 0 || unsafe.getLong(current + Byte.BYTES) < bytes)){
+            current = unsafe.getLong(current + Byte.BYTES + 2*Long.BYTES);
         }
         if (current >= baseAddress + totalSize) throw new OutOfMemoryError("No blocks fit request");
         unsafe.putByte(current, (byte) 0);
+        if (unsafe.getLong(current + Byte.BYTES) >= bytes + (Byte.BYTES+3*Long.BYTES) + minSize){
+            unsafe.putByte(current + Byte.BYTES + 3*Long.BYTES + bytes, (byte) 1);
+            unsafe.putLong(current + 2*Byte.BYTES + 3*Long.BYTES + bytes, unsafe.getLong(current + Byte.BYTES) - bytes - (Byte.BYTES + 3*Long.BYTES));
+            unsafe.putLong(current + 2*Byte.BYTES + 4*Long.BYTES + bytes, current);
+            unsafe.putLong(current + 2*Byte.BYTES + 5*Long.BYTES + bytes, unsafe.getLong(current + Byte.BYTES + 2*Long.BYTES));
+
+            unsafe.putLong(current + Byte.BYTES + 2*Long.BYTES, current + Byte.BYTES + 3*Long.BYTES + bytes);
+            if (unsafe.getLong(current + 2*Byte.BYTES + 5*Long.BYTES + bytes) < baseAddress + totalSize){
+                unsafe.putLong(unsafe.getLong(current + 2*Byte.BYTES + 5*Long.BYTES + bytes) + Byte.BYTES + Long.BYTES, current + Byte.BYTES + 3*Long.BYTES + bytes);
+            }
+        }
         return current + Byte.BYTES + 3*Long.BYTES;
     }
 
